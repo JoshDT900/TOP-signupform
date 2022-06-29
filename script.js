7930cd012a8f43d9874aa29352abbe5bf9d7e03e
JoshDT900 <DJTroutman900@gmail.com>
2022-06-29 18:10:47 -0400

    Add some validation logic and errors

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 let createPass = document.querySelector("#password")
 let confirmPass = document.querySelector("#confirm_password")
+let confirmTxt = document.querySelectorAll(".confirmText")
 let submitBtn = document.querySelector(".submit_btn")
 
 let checkIfSame = () => {
-  console.log("Works?");
   if (createPass.value != confirmPass.value) {
-  return alert("WRONG@@!!")
+  alert("Passwords don't match")
+  confirmTxt.forEach(target => target.textContent = "*Passwords don't match")
+  createPass.value = "";
+  return confirmPass.value = "";
 }
 }
 
-submitBtn.addEventListener('click', checkIfSame)
+confirmPass.addEventListener('blur', checkIfSame)
 
